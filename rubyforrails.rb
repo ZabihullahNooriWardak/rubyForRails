68f35f424bf38c50084afc4021f14b8556bd16f8
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-11-23 10:00:46 +0430

    custom methods for getting the elements from the first and last of the array like : arr.first   arr.first(4)

--- rubyforrails.rb
+++ rubyforrails.rb
@@ -1,12 +1,35 @@
 
-number = 1
-if number%2==0
-    puts "it is even"
-    if number==10
-puts "it is also ten"
-    end
-elsif number%2!=0
-    puts "no even"
-    
-
+arr = [1,2,3,4,5,6,7,8,9,10]
+def custom_first(arr,num=0)
+if num==0
+ return arr[0]
 end
+custom_array = [];
+custom_num =0;
+while custom_num<num
+    custom_array[custom_array.length]=arr[custom_num]
+    custom_num +=1
+end
+custom_array
+end
+
+
+ p custom_first(arr)
+
+
+
+def custom_last(arr,num=0)
+    if num==0
+        return arr[arr.length-1]
+       end
+       custom_array = [];
+       custom_num =0;
+       while num>0
+           custom_array[custom_array.length]=arr[arr.length-num]
+         num-=1
+       end
+       custom_array
+end
+
+p custom_last(arr,4)
+p arr
