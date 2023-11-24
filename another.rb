274f4bb02b070d050a6d52ae6f5d1e9a9c5786df
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-11-24 19:38:56 +0430

    solved

--- another.rb
+++ another.rb
@@ -2,6 +2,7 @@ sentence = "once upon a time in a land far far away "
 def word_count (string)
 arr = string.split(" ")
 new_nested_array = []
+hash_for_return = {}
 arr.each do |i|
   occurence = 0 
 arr.each do |j|
@@ -11,7 +12,12 @@ end
 end
 new_nested_array.push({i=>occurence}) 
 end
- new_nested_array.uniq
+ new_nested_array = new_nested_array.uniq
+new_nested_array.each_with_index do |element,index|
+
+  hash_for_return.merge!(element)
+end 
+hash_for_return
 end 
 
 p word_count(sentence)
